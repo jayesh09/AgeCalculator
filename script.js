465512a96815a12b3cced952a88976d348ef49aa
jayesh09 <77573909+jayesh09@users.noreply.github.com>
2021-11-22 13:58:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+function ageCalculate(){
+    let today = new Date();
+    let inputDate1 = new Date(document.getElementById("date-input1").value);
+    let inputDate2 = new Date(document.getElementById("date-input2").value);
+    let birthMonth,birthDate,birthYear;
+    //copying value
+    let birthDetails = {
+        date:inputDate1.getDate(),
+        month:inputDate1.getMonth()+1,
+        year:inputDate1.getFullYear()
+    }
+    let currentYear = inputDate2.getFullYear();
+    let currentMonth = inputDate2.getMonth()+1;
+    let currentDate = inputDate2.getDate();
+
+    leapChecker(currentYear);
+    //not born
+    if(
+        birthDetails.year > currentYear ||
+        ( birthDetails.month > currentMonth && birthDetails.year == currentYear) || 
+        (birthDetails.date > currentDate && birthDetails.month == currentMonth && birthDetails.year == currentYear)
+    ){
+        alert("Not Born Yet");
+        displayResult("-","-","-");
+        return;
+    }
+
+    birthYear = currentYear - birthDetails.year;
+
+    if(currentMonth >= birthDetails.month){
+        birthMonth = currentMonth - birthDetails.month;
+    }
+    else{
+        birthYear--;
+        birthMonth = 12 + currentMonth - birthDetails.month;
+    }
+
+    if(currentDate >= birthDetails.date){
+        birthDate = currentDate - birthDetails.date;
+    }
+    else{
+        birthMonth--;
+        let days = months[currentMonth - 2];
+        birthDate = days + currentDate - birthDetails.date;
+        if(birthMonth < 0){
+            birthMonth = 11;
+            birthYear--;
+        }
+    }
+    displayResult(birthDate,birthMonth,birthYear);
+}
+//changing value
+function displayResult(bDate,bMonth,bYear){
+    document.getElementById("years").textContent = bYear;
+    document.getElementById("months").textContent = bMonth;
+    document.getElementById("days").textContent = bDate;
+}
+//leap year
+function leapChecker(year){
+    if(year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)){
+        months[1] = 29;
+    }
+    else{
+        months[1] = 28;
+    }
+}
