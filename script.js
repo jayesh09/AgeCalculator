c7a184ad2b26ed74c87266b6dc7c1948897cb427
jayesh09 <77573909+jayesh09@users.noreply.github.com>
2021-11-29 09:58:58 +0530

    Disable future

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 
+//Disable future
+var today = new Date();
+    var maxDate = today.getDate();
+    var maxMonth = today.getMonth()+1;
+    var maxYear = today.getFullYear();
+
+    var maxTime = maxYear+"-"+maxMonth+"-"+maxDate;
+    
+    document.getElementById("date-input1").setAttribute("max", maxTime);
+    document.getElementById("date-input2").setAttribute("max", maxTime);
+
 function ageCalculate(){
     let today = new Date();
     let inputDate1 = new Date(document.getElementById("date-input1").value);
